d5b6143947e726281bdc29e84e2210c94817e56f
unknown <ullhaq@hotmail.com>
2020-02-21 08:57:39 +0100

    Session Model updated

--- app/Session.php
+++ app/Session.php
@@ -7,24 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Session extends Model
 {
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
      * Get the action record associated with the user.
      */
     public function actions()
