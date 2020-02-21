709c9a0fdd0363069f8f85f93b3a6a293fd7b5dc
unknown <ullhaq@hotmail.com>
2020-02-21 08:02:44 +0100

    migrations added

---
+++ database/migrations/2020_02_20_121929_create_actions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('location');
+            $table->string('action');
+            $table->string('target');
+            $table->unsignedBigInteger('session_id');
+            $table->timestamps();
+
+            $table->foreign('session_id')->references('id')->on('sessions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actions');
+    }
+}
--- database/migrations/2020_02_20_123518_create_variables_table.php
+++ database/migrations/2020_02_20_123518_create_variables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateVariablesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('variables', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('variable');
+            $table->string('value');
+            $table->unsignedBigInteger('action_id');
             $table->timestamps();
+
+            $table->foreign('action_id')->references('id')->on('actions')->onDelete('cascade');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('variables');
     }
 }
