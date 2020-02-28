c897e6e3223cf29df989353c6c17c8d27e27d802
unknown <ullhaq@hotmail.com>
2020-02-28 16:43:51 +0100

    page titled updated

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ env("APP_NAME", "Laravel") }}</title>
 
         <link href="{{ asset('css/app.css')}}" rel="stylesheet">
     </head>
