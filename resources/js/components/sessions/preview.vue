3b9d35f2dc80e67040a618ad0362df87a361633c
unknown <ullhaq@hotmail.com>
2020-03-02 09:43:51 +0100

    variables inside string

--- resources/js/components/sessions/preview.vue
+++ resources/js/components/sessions/preview.vue
@@ -44,7 +44,7 @@
               </tr>
             </thead>
             <tbody v-for="action in entry.actions" :key="action.id">
-              <tr class="pointer" data-toggle="collapse" :data-target="'#variableDetails' + action.id" aria-expanded="false" :aria-controls="'#variableDetails' + action.id">
+              <tr class="pointer" data-toggle="collapse" :data-target="`#variableDetails${action.id}`" aria-expanded="false" :aria-controls="`#variableDetails${action.id}`">
                 <td><span class="badge badge-secondary">{{action.id}}</span></td>
                 <td>{{action.location}}</td>
                 <td>{{action.action}}</td>
@@ -52,27 +52,25 @@
                 <td>{{action.variables.length}}</td>
                 <td>{{moment(action.created_at).fromNow()}}</td>
               </tr>
-              <tr class="collapse" :id="'variableDetails' + action.id">
+              <tr class="collapse" :id="`variableDetails${action.id}`">
                 <td colspan="6">
-                  <div >
-                    <div class="card card-body">
-                      <table class="table table-borderless">
-                        <thead>
-                          <tr>
-                            <th>#</th>
-                            <th>Variable</th>
-                            <th>Value</th>
-                          </tr>
-                        </thead>
-                        <tbody v-for="variable in action.variables" :key="variable.id">
-                          <tr>
-                            <td><span class="badge badge-secondary">{{variable.id}}</span></td>
-                             <td>{{variable.variable}}</td>
-                            <td>{{variable.value}}</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
+                  <div class="card card-body">
+                    <table class="table table-borderless">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Variable</th>
+                          <th>Value</th>
+                        </tr>
+                      </thead>
+                      <tbody v-for="variable in action.variables" :key="variable.id">
+                        <tr>
+                          <td><span class="badge badge-secondary">{{variable.id}}</span></td>
+                            <td>{{variable.variable}}</td>
+                          <td>{{variable.value}}</td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </td>
               </tr>
