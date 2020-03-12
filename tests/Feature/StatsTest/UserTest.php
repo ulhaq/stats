ea4c824d4eb99f745b083ca0d0a64377fbb71261
unknown <ullhaq@hotmail.com>
2020-03-12 13:08:08 +0100

    Test for sessions between dates added

--- tests/Feature/StatsTest/UserTest.php
+++ tests/Feature/StatsTest/UserTest.php
@@ -51,4 +51,22 @@ class UserTest extends TestCase
             ->assertStatus(200)
             ->assertJson($sessions);
     }
+
+    /** @test */
+    public function a_user_can_get_a_list_of_all_sessions_between_dates_belonging_to_a_user()
+    {
+        $this->withoutExceptionHandling();
+
+        $session = factory(Session::class)->create(["user" => 15, "created_at" => "2020-03-08 12:00:00"]);
+        unset($session->user);
+        unset($session->updated_at);
+        factory(Session::class)->create(["user" => 15, "created_at" => "2020-03-12 12:00:00"]);
+        
+
+        $response = $this->json("GET", "api/stats/users/15?from=2020-03-08T11:00&to=2020-03-12T11:59");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([$session->toArray()])->assertDontSee("2020-03-12");
+    }
 }
