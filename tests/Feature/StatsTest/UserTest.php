46af8c055e594b79b3d021cd0029e1a8734748d9
unknown <ullhaq@hotmail.com>
2020-03-13 09:52:08 +0100

    test for users with returns x times between dates

--- tests/Feature/StatsTest/UserTest.php
+++ tests/Feature/StatsTest/UserTest.php
@@ -23,33 +23,70 @@ class UserTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJson([
-                "counts" => [
+                "data" => [
                     [
                         "user" => $sessions[0]->user,
                         "total" => 10,
                     ],
-                ]
+                ],
             ]);
     }
 
     /** @test */
-    public function a_user_can_get_the_percentage_of_users_who_return_at_least_x_times()
+    public function a_user_can_get_a_list_of_the_amount_of_sessions_of_users_who_returned_at_least_x_times_between_dates()
     {
         $this->withoutExceptionHandling();
 
         $sessions = [
-            factory(Session::class, 2)->create(["user" => 1]),
-            factory(Session::class, 5)->create(["user" => 2]),
-            factory(Session::class, 4)->create(["user" => 3]),
-            factory(Session::class, 3)->create(["user" => 4]),
-            factory(Session::class, 10)->create(["user" => 5]),
-            factory(Session::class, 7)->create(["user" => 6]),
+            factory(Session::class, 2)->create(["user" => 1, "created_at" => "2020-03-07 11:00:00"]),
+            factory(Session::class, 5)->create(["user" => 2, "created_at" => "2020-03-09 11:00:00"]),
+            factory(Session::class, 4)->create(["user" => 3, "created_at" => "2020-03-10 11:00:00"]),
+            factory(Session::class, 3)->create(["user" => 4, "created_at" => "2020-03-08 11:00:00"]),
+            factory(Session::class, 10)->create(["user" => 5, "created_at" => "2020-03-11 11:00:00"]),
+            factory(Session::class, 7)->create(["user" => 6, "created_at" => "2020-03-11 12:00:00"]),
+        ];
+        
+        $response = $this->json("GET", "api/stats/users/login?times=3&from=2020-03-08T08:00&to=2020-03-11T11:59");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                "current_page" => 1,
+                "data" => [
+                    [
+                        "user" => 5,
+                        "total" => 10,
+                    ],
+                    [
+                        "user" => 2,
+                        "total" => 5,
+                    ],
+                    [
+                        "user" => 3,
+                        "total" => 4,
+                    ],
+                ],
+            ]);
+    }
+
+    /** @test */
+    public function a_user_can_get_the_percentage_of_users_who_returned_at_least_x_times_between_dates()
+    {
+        $this->withoutExceptionHandling();
+
+        $sessions = [
+            factory(Session::class, 2)->create(["user" => 1, "created_at" => "2020-03-07 11:00:00"]),
+            factory(Session::class, 5)->create(["user" => 2, "created_at" => "2020-03-09 11:00:00"]),
+            factory(Session::class, 4)->create(["user" => 3, "created_at" => "2020-03-10 11:00:00"]),
+            factory(Session::class, 3)->create(["user" => 4, "created_at" => "2020-03-08 11:00:00"]),
+            factory(Session::class, 10)->create(["user" => 5, "created_at" => "2020-03-11 11:00:00"]),
+            factory(Session::class, 7)->create(["user" => 6, "created_at" => "2020-03-11 12:00:00"]),
         ];
 
-        $response = $this->json("GET", "api/stats/users/returning?times=3");
+        $response = $this->json("GET", "api/stats/users/returning?times=3&from=2020-03-08T08:00&to=2020-03-11T11:59");
 
         $totalUsers = sizeof($sessions);
-        $totalReturningUsers = 4;
+        $totalReturningUsers = 3;
 
         $percentage = $totalReturningUsers / $totalUsers * 100;
 
